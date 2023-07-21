e87fe5cde47b4549f46855e17f932de88cd71704
allex8402 <120774992+allex8402@users.noreply.github.com>
2023-07-21 14:12:38 +0800

    12 пр третья итерация, первая загрузка

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ImagePopup({ card, onClose }) {
     return (
-        <div className={`popup popup_type_image ${card ? 'popup_opened' : ''}`}>
+        <div className={`popup popup-image ${card ? 'popup_opened' : ''}`}>
             <div className=" popup-image__container ">
                 <button className="popup__close-icon" type="button" onClick={() => onClose(false)}></button>
                 <img className="popup-image__img" src={card ? card.link : ''} alt={card ? card.name : ''} />
